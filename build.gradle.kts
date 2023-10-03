eb5d70a12a1830bf88c6fdbc77f5ff5246fb3c7a
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-02 23:52:01 -0400

    work?

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ rootProject.version = "1.0"
 rootProject.group = "me.badbones69.crazyrunes"
 
 dependencies {
-    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.20.2-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -19,11 +19,25 @@ tasks {
     }
 
     assemble {
+        val jarsDir = File("$rootDir/jars")
+        if (jarsDir.exists()) jarsDir.delete()
+
         dependsOn(reobfJar)
+
+        doLast {
+            if (!jarsDir.exists()) jarsDir.mkdirs()
+
+            val file = file(reobfJar.get().outputJar.get())
+
+            copy {
+                from(file)
+                into(jarsDir)
+            }
+        }
     }
 
     runServer {
-        minecraftVersion("1.20.1")
+        minecraftVersion("1.20.2")
 
         jvmArgs("-Dnet.kyori.ansi.colorLevel=truecolor")
     }
